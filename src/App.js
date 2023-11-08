478f70de44e419761d5ea47a26b2a60b331afeb3
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-08 15:54:13 -0500

    fix issues and errors with deleting no need from default

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './Components/Navbar';
-import Sidebar from "./Components/Sidebar";
+import Sidebar from './Components/Sidebar';
 
+function App() {
+  return (
+    <React.StrictMode>
+      <Navbar />
+      <Sidebar />
+    </React.StrictMode>
+  );
+}
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Sidebar />
-  </React.StrictMode>
-);
+export default App;
