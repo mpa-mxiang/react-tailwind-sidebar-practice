c648f229fbdf1cc68424d99487cbf16ed35188e3
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-11-14 22:53:25 -0500

    add sidebar component in app to see how it looks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Sidebar from "./Components/Sidebar"
 import "./App.css";
 function App() {
-    return <div className="app"></div>
+    return <div className="app">
+      <Sidebar />
+    </div>
 }
 
 export default App
